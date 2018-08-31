8d18185aef3b1d41864e9780d075fc78c7dd1a6c
Isaacb22 <ibonalde001@gmail.comgit config --global user.name Isaacb22git config --global user.email ibonalde001@gmail.com>
2018-08-31 13:01:42 -0400

    Agregar menu

--- cinex.py
+++ cinex.py
@@ -1,22 +1,31 @@
-print ("\n\n\t  Bienvenido al control administrativo del cine VERGATARIO")
+def control_de_salas():
+		numero_de_salas = input("\nIngrese el numero de salas: ")
+		lista_salas = []
+	
+		for i in range(1,int(numero_de_salas)+1):
+			obj_sala = {}
+			limite_sala = input("Ingrese el limite para la sala {}:".format(i))
+
+			obj_sala['nro_sala'] = i
+			obj_sala['limite_sala'] = limite_sala
+
+			lista_salas.append(obj_sala)
+			obj_sala = {}   
+		return lista_salas
+
 
 
 def menu():
-	pass
+	print ("\n\n\t\t\t  Bienvenido al cine VERGATARIO")
+	print ("\n ||| 1)control de salas ||| \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ ||| 2)control de clientes VIP |||")
+	print ("\n \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ ")
+	print ("\n 	\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ ||| 3)control de clientes normal  ||| \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ ")
+	selecciona_una_opcion = input("\nSeleccione una opcion:")
 	
+	if selecciona_una_opcion == '1':
+		print ("\nTu has elegido la opcion Control de salas")
+		control_de_salas()
 
-def control_de_salas():
-	numero_de_salas = input("\nIngrese el numero de salas: ")
-	lista_salas = []
-	for i in range(1,int(numero_de_salas)+1):
-		obj_sala = {}
-		limite_sala = input("Ingrese el limite para la sala {}:".format(i))
 
-		obj_sala['nro_sala'] = i
-		obj_sala[limite_sala] = limite_sala
-
-		lista_salas.append(obj_sala)
-		obj_sala = {}	
-	return lista_salas
-
-control_de_salas()
+	
+menu()
