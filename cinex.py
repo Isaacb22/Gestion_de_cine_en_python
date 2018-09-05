1a134a49122e487d23dce190a8e455073777c09d
Isaacb22 <ibonalde001@gmail.comgit config --global user.name Isaacb22git config --global user.email ibonalde001@gmail.com>
2018-09-05 13:11:54 -0400

    Validacion de solo numeros

--- cinex.py
+++ cinex.py
@@ -16,32 +16,61 @@ def menu():
 	print (" |||====================||||||||||||||||||||||||||||||===============================||||||||||||||||||||||||||===========================|||")
 	print (" ||||||||||||||||||||||||||||||||||||||||||||||||||||| 3)control de clientes normal  ||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
 	print (" ============================================================================================================================================")
-	selecciona_una_opcion = input("\nSeleccione una opcion:")
+	selecciona_una_opcion = input("\nSeleccione una opcion: ")
 	
 	if selecciona_una_opcion == '1':
 		print ("\nTu has elegido la opcion Control de salas")
 		control_de_salas()
 
 
+
 	
 def control_de_salas():
-		numero_de_salas = input("\nIngrese el numero de salas: ")
-		lista_salas = []
+	while True:
+		try:
+			numero_de_salas = int(input("\nIngrese el numero de salas: "))
+			break	
 	
-		for i in range(1,int(numero_de_salas)+1):
-			obj_sala = {}
-			limite_sala = input("Ingrese el limite para la sala {}:".format(i))
-			clientes_vip = input("Ingrese la cantidad de clientes VIP de la sala {}".format(i))
-			clientes_normales = input("Ingrese la cantidad de clientes normales de la sala :{}".format(i))
-			obj_sala['nro_sala'] = i
-			obj_sala['limite_sala'] = limite_sala
-			obj_sala['clientes_vip'] = clientes_vip
-			while validacion_de_porcentaje(limite_sala,clientes_vip) == False:
-				print ("Limite de asientos VIP excedido")
-				clientes_vip = input("Ingrese la cantidad de clientes VIP de la sala {}".format(i))
-			obj_sala['clientes_normales'] = clientes_normales
-			lista_salas.append(obj_sala)
-			obj_sala = {}   
-		return lista_salas
+		except ValueError:
+			print ('\nCaracter invalido, solo se permiten numeros')
+	
+	lista_salas = []
+	
+	for i in range(1,int(numero_de_salas)+1):
+		obj_sala = {}
+		
+		while True:
+			try:
+				limite_sala = int(input("\nIngrese el limite para la sala {}: ".format(i)))
+				break
+			
+			except ValueError:
+				print ('\nCaracter invalido, solo se permiten numeros')	
+		
+		while True:
+			try:
+				clientes_vip = int(input("\nIngrese la cantidad de clientes VIP de la sala {}: ".format(i)))
+				break
+			
+			except ValueError:
+				print ('\nCaracter invalido, solo se permiten numeros')		
+		
+		while True:
+			try:
+				clientes_normales = int(input("\nIngrese la cantidad de clientes normales de la sala {}: ".format(i)))
+			
+			except ValueError:
+				print ('\nCaracter invalido, solo se permiten numeros')
 
+		obj_sala['nro_sala'] = i
+		obj_sala['limite_sala'] = limite_sala
+		obj_sala['clientes_vip'] = clientes_vip
+		while validacion_de_porcentaje(limite_sala,clientes_vip) == False:
+			print ("Limite de asientos VIP excedido")
+			clientes_vip = input("Ingrese la cantidad de clientes VIP de la sala {}: ".format(i))
+
+		obj_sala['clientes_normales'] = clientes_normales
+		lista_salas.append(obj_sala)
+		obj_sala = {}   
+	
 menu()
