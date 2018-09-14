d09841878fa6a134e7a4e6f2a97d1227775a2157
Isaacb22 <ibonalde001@gmail.comgit config --global user.name Isaacb22git config --global user.email ibonalde001@gmail.com>
2018-09-14 11:28:59 -0400

    funcion Control de salas terminada

--- cinex.py
+++ cinex.py
@@ -1,12 +1,8 @@
+import getpass
 porcentaje_de_clientes_vip = 20
 lista_salas = []
-config = {
-	'cont_vip': 0,
-	'cont_normal': 0
-	'limite_sala':0	
 
-}
- 
+salas = {}
 
 
 def eleccion_cliente(config):
@@ -20,9 +16,11 @@ def eleccion_cliente(config):
 					config['cont_normal'] += 1
 		except ValueError:
 			print ('\nCaracter invalido, solo se permiten numeros')
-		print (config["cont_vip"])
-		print (config["cont_normal"])
 		
+	
+	while validacion_de_porcentaje(config["limite_sala"],config["cont_vip"]) == False:
+			print ("Limite de asientos VIP excedido")
+			config["cont_vip"] = input("Ingrese la cantidad de clientes VIP de la sala {}: ".format(i))
 
 
 def validacion_de_porcentaje(limite_sala,clientes_vip):
@@ -33,58 +31,74 @@ def validacion_de_porcentaje(limite_sala,clientes_vip):
 		return False
 
 
-
 def menu():
+	
 	print ("\n\n\t\t\t\t\t\t\t  Bienvenido al cine VERGATARIO")
-	print (" ============================================================================================================================================")
-	print (" ||| 1)control de salas ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| 2)control de clientes VIP |||")
-	print (" |||====================||||||||||||||||||||||||||||||===============================||||||||||||||||||||||||||===========================|||")
-	print (" ||||||||||||||||||||||||||||||||||||||||||||||||||||| 3)control de clientes normal  ||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
-	print (" ============================================================================================================================================")
+	print (" =============================================================================================================================================")
+	print (" |||  1)Control de salas   |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||    2)Control de clientes  |||")
+	print (" |||=======================|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||===========================|||")
+	print (" |||	  3)Resultados     |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||    4)Salir del programa   |||")
+	print (" =============================================================================================================================================")
 	selecciona_una_opcion = input("\nSeleccione una opcion: ")
 	
 	if selecciona_una_opcion == '1':
 		print ("\nTu has elegido la opcion Control de salas")
-		control_de_salas(config)
-
-
-
+		
+		nombre_de_usuario = input("\nIngrese su nombre de usuario: ")
+		contrasena = getpass.getpass('\nIngrese su contrasena: ')
+		while True:
+			if nombre_de_usuario != 'isaacb22' or contrasena != '1234':
+				print ('\nInicio de sesion fallido. Intente otra vez')
+				nombre_de_usuario = input("\nIngrese su nombre de usuario: ")
+				contrasena = getpass.getpass('\nIngrese su contrasena: ')
+			else:
+				print ("\nInicio de sesion exitoso")
+				control_de_salas(config)
+				break
+			
+	else:
+		if selecciona_una_opcion == '2':
+			print ("\nTu has elegido la opcion Contro de clientes")
+			eleccion_cliente(config)
+	
+	
 	
 def control_de_salas(config):
 	while True:
 		try:
-			nro_sala = int(input("\nIngrese el numero de salas: "))
+			nro_sala = int(input("\n\nIngrese el numero de salas: "))
 			break	
 	
 		except ValueError:
 			print ('\nCaracter invalido, solo se permiten numeros')
 	
 	
-	
 	for i in range(1,int(nro_sala)+1):
 		
-		
+
 		while True:
 			try:
-				config['limite_sala'] = int(input("\nIngrese el limite para la sala {}: ".format(i)))
-				
+				limite_sala = int(input("\nIngrese el limite para la sala {}: ".format(i)))
+				config['limite_sala'] = limite_sala
+				nombre_de_salas = "sala{}".format(i)
+				salas[nombre_de_salas] = {"limite" : limite_sala}
 				break
 			
+
 			except ValueError:
-				print ('\nCaracter invalido, solo se permiten numeros')	
-		
-		eleccion_cliente(config)
-		
-		
-		config['num_sala'] = i
-		
-		
-		while validacion_de_porcentaje(config["limite_sala"],config["cont_vip"]) == False:
-			print ("Limite de asientos VIP excedido")
-			config["cont_vip"] = input("Ingrese la cantidad de clientes VIP de la sala {}: ".format(i))
+				print ('\nCaracter invalido, solo se permiten numeros')
 
 		
-		lista_salas.append(config)
-		config = {}  
+	print ("\n\t\t\t\t\t     Has validado la opcion Control de salas exitosamente")
 	
-menu()
+		
+	config['num_sala'] = i
+		
+		
+
+		
+	lista_salas.append(config)
+	config = {}  
+
+while True:
+	menu()
