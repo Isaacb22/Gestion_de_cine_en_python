b9bbc1bb567857c97e58a9ad6336bdc42919ec3b
Isaacb22 <ibonalde001@gmail.comgit config --global user.name Isaacb22git config --global user.email ibonalde001@gmail.com>
2018-09-26 12:32:46 -0400

    validacion del 25% de clientes vip completada

--- cinex.py
+++ cinex.py
@@ -16,7 +16,7 @@ def mostrar_salas():
 
 
 
-def eleccion_cliente(normal,vip,nombre):
+def eleccion_cliente():
 
 	while True:
 		try:
@@ -30,24 +30,26 @@ def eleccion_cliente(normal,vip,nombre):
 			elegir_cliente = int(input('\nQue tipo de cliente quiere ser? \n\n\t\t\t1)VIP  \t\t\t\t\t\t2)NORMAL : '))
 			
 			for i in range(len(salas)):
+				
 				if entrar_sala in salas[i] and elegir_cliente == 1:
 					if validacion_de_porcentaje(salas[i][3]) == False:
-						print ("Limite de asientos VIP excedido")
+						print ("\n\nLimite de asientos VIP excedido\n")
+						
 					else:
 						salas[i][1] += 1
-					
+						print (salas[i][1])
 				if entrar_sala in salas[i] and elegir_cliente == 2:
 					salas[i][2] += 1
 					
 		except ValueError:
 			print ('\nCaracter invalido, solo se permiten numeros')
 		
-		break
+		
 	
-	while validacion_de_porcentaje(5) == False:
-			print ("Limite de asientos VIP excedido")
+	#while validacion_de_porcentaje(5) == False:
+			#print ("\n\nLimite de asientos VIP excedido\n")
 			
-			elegir_cliente = int(input('\nQue tipo de cliente quiere ser? \n\n\t\t\t1)VIP  \t\t\t\t\t\t2)NORMAL : '))
+			#elegir_cliente = int(input('\nQue tipo de cliente quiere ser? \n\n\t\t\t1)VIP  \t\t\t\t\t\t2)NORMAL : '))
 
 
 
@@ -55,9 +57,10 @@ def eleccion_cliente(normal,vip,nombre):
 def validacion_de_porcentaje(limite):
 	
 	porcentaje_total = int(limite) * int(porcentaje_de_clientes_vip) / 100
+	
 	for i in range(len(salas)):
-		
-		if salas[i][1] > int(porcentaje_total):
+	
+		if salas[i][1] + 1 > int(porcentaje_total):
 			return 	False
 		else:
 			return True
@@ -92,7 +95,7 @@ def menu():
 	
 	if selecciona_una_opcion == '2':
 		print ("\nTu has elegido la opcion Contro de clientes")
-		eleccion_cliente(cont_normal,cont_vip,nombre_de_sala)
+		eleccion_cliente()
 	
 	
 	
